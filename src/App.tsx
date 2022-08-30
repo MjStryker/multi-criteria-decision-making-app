7372b3e7ac8fd47adb0ecba2fab748f0f2ff647e
MjStryker <mjstryker51@gmail.com>
2022-08-30 22:36:49 +0200

    First commit from laptop

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import DataTable from "./components/DataTable";
+
 function App() {
-  return <div className="App">MCDMA</div>;
+  return (
+    <div className="App">
+      MCDMA
+      <DataTable />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/DataTable.tsx
@@ -0,0 +1,11 @@
+type DataTableProps = {};
+
+const DataTable = (props: DataTableProps) => {
+  const criteriaList = [];
+
+  const productList = [];
+
+  return <></>;
+};
+
+export default DataTable;
