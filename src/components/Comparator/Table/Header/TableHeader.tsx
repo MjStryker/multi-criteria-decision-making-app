18bfd514070cda4f26b09e563fa95321e2500bde
MjStryker <mjstryker51@gmail.com>
2023-01-21 16:21:08 +0100

    Reworks Table Header

--- src/components/Comparator/Table/Header/TableHeader.tsx
+++ src/components/Comparator/Table/Header/TableHeader.tsx
@@ -1,11 +1,22 @@
-import { Button, Flex, Td, Thead, Tr } from "@chakra-ui/react";
-import { useRef, useState } from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  IconButton,
+  Tag,
+  Td,
+  Text,
+  Thead,
+  Tr,
+  VStack,
+} from "@chakra-ui/react";
 
+import { AddIcon } from "@chakra-ui/icons";
 import { CRITERION } from "../../../../constants/criteria";
 import EditProductButton from "./EditProductButton";
 import { TProduct } from "../../../../types/products";
 import { createEmptyProduct } from "../../../../utils/products/products";
-import useClickOutside from "../../../../hooks/useClickOutside";
 
 type TableHeaderProps = {
   products: TProduct[];
@@ -20,18 +31,9 @@ const TableHeader = ({
   updateProduct,
   removeProduct,
 }: TableHeaderProps) => {
-  const inputRef = useRef(null);
-
-  const [cellId, setCellId] = useState<string | null>(null);
-  const [cellValue, setCellValue] = useState<string | number | boolean | null>(
-    null
-  );
-
-  useClickOutside(inputRef, () => {
-    console.log(`[ Cell ] Clicked away from ${cellId}..`);
-    setCellId(null);
-    setCellValue(null);
-  });
+  const handleAddProduct = () => {
+    addProduct(createEmptyProduct(products.length));
+  };
 
   return (
     <Thead>
@@ -62,112 +64,29 @@ const TableHeader = ({
          * PRODUCTS
          */}
         {products.map((product, idx) => {
-          const isCellInEditMode = product.id === cellId;
-
           return (
             <Td key={product.id}>
-              <div
-                className="HeadCellProductContainer"
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "space-between",
-                  height: "100%",
-                }}
-              >
-                <Flex
+              <VStack className="CellContainer" alignItems="stretch">
+                <HStack
                   className="FirstRowContainer"
                   flex={1}
                   flexDirection="row"
                   justifyContent="space-between"
                   alignItems="center"
                 >
-                  <div
-                    ref={isCellInEditMode ? inputRef : undefined}
-                    onClick={() => {
-                      if (isCellInEditMode) {
-                        return;
-                      }
-                      console.log(`[ Cell ] Selected ${product.id}`);
-                      setCellId(product.id);
-                      setCellValue(product.name ?? null);
-                    }}
-                    onBlur={() => {
-                      updateProduct({
-                        ...product,
-                        name: cellValue ? `${cellValue}` : undefined,
-                      });
-                      setCellId(null);
-                      setCellValue(null);
-                    }}
-                    style={{
-                      display: "flex",
-                      alignItems: "stretch",
-                      padding: isCellInEditMode ? 0 : 8,
-                      width: "100%",
-                    }}
-                  >
-                    {isCellInEditMode ? (
-                      <input
-                        type="text"
-                        value={cellValue ? `${cellValue}` : ""}
-                        onChange={(e) => {
-                          const value = e.target.value;
-                          const newValue =
-                            value && value.length > 0 ? value : null;
+                  <Text>{product.name ?? `Produit ${idx + 1}`}</Text>
 
-                          setCellValue(newValue);
-                          updateProduct({
-                            ...product,
-                            name: newValue ? `${newValue}` : undefined,
-                          });
-                        }}
-                        onKeyDown={(e) => {
-                          if (e.key === "Enter") {
-                            updateProduct({
-                              ...product,
-                              name: cellValue ? `${cellValue}` : undefined,
-                            });
-                            setCellId(null);
-                            setCellValue(null);
-                          }
-                        }}
-                        style={{
-                          width: "100%",
-                          padding: "7px 6px",
-                        }}
-                      />
-                    ) : (
-                      <div
-                        className="ProductName"
-                        style={{ flex: 1, alignSelf: "center", marginRight: 8 }}
-                      >
-                        {product.name ?? `Produit ${idx + 1}`}
-                      </div>
-                    )}
-                  </div>
+                  <EditProductButton
+                    product={product}
+                    updateProduct={updateProduct}
+                    removeProduct={removeProduct}
+                  />
+                </HStack>
 
-                  {!isCellInEditMode && (
-                    <EditProductButton
-                      product={product}
-                      updateProduct={updateProduct}
-                      removeProduct={removeProduct}
-                    />
-                  )}
-                </Flex>
-
-                <div
-                  className="HeadCellProductRow-2"
-                  style={{
-                    padding: "6px 8px",
-                    fontSize: "12px",
-                    color: "#5a5a5a",
-                    backgroundColor: "rgba(0, 0, 0, 0.02",
-                  }}
-                >
-                  Column idx: <b>{product.defaultColumnIdx}</b>
-                </div>
-              </div>
+                <Tag color="gray.600" fontSize="xs">
+                  {product.reference}
+                </Tag>
+              </VStack>
             </Td>
           );
         })}
@@ -175,12 +94,14 @@ const TableHeader = ({
         {/*
          * PRODUCTS - ADD BUTTON
          */}
-        <Td>
-          <Button
-            onClick={() => addProduct(createEmptyProduct(products.length))}
-          >
-            +
-          </Button>
+        <Td border="none">
+          <IconButton
+            colorScheme="blue"
+            aria-label="Add product"
+            size="sm"
+            icon={<AddIcon />}
+            onClick={handleAddProduct}
+          />
         </Td>
       </Tr>
     </Thead>
