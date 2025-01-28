66b5fe368286768c66d952f7299d4e906630b6da
MjStryker51 <mjstryker51@gmail.com>
2025-01-28 13:50:34 +0100

    Rename ProductAddedEvent payload prop with detail

--- src/Application/Events/ProductAdded.event.ts
+++ src/Application/Events/ProductAdded.event.ts
@@ -2,7 +2,7 @@ import { AbstractEvent } from '@/@Event/AbstractEvent';
 import { ProductDto } from '../Dtos/Product.dto';
 
 export class ProductAddedEvent extends AbstractEvent<ProductDto> {
-  constructor(public payload: ProductDto) {
-    super(ProductAddedEvent.name);
+  constructor(detail: ProductDto) {
+    super(ProductAddedEvent.name, detail);
   }
 }
