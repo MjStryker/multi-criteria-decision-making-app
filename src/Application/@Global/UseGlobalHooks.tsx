f3570ce59d1c332c6fc24295f3d5721e832618f5
MjStryker <mjstryker51@gmail.com>
2025-01-30 22:03:12 +0100

    Fix import

--- src/Application/@Global/UseGlobalHooks.tsx
+++ src/Application/@Global/UseGlobalHooks.tsx
@@ -1,5 +1,5 @@
-import UseOnCriterionAddedEventAddDefaultProductCriterionValuesListener from '../Criterion/Listeners/UseOnCriterionAddedEventAddDefaultProductCriterionValues.listener';
-import UseOnProductAddedEventAddDefaultProductCriterionValuesListener from '../ProductCriterionValue/Listeners/UseOnProductAddedEventAddDefaultProductCriterionValues.listener';
+import UseOnCriterionAddedEventAddDefaultProductCriterionValuesListener from '@/Application/ProductCriterionValue/Listeners/UseOnCriterionAddedEventAddDefaultProductCriterionValues.listener';
+import UseOnProductAddedEventAddDefaultProductCriterionValuesListener from '@/Application/ProductCriterionValue/Listeners/UseOnProductAddedEventAddDefaultProductCriterionValues.listener';
 
 export default function UseGlobalHooks() {
   /**
