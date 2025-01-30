f80f5623d43af93f9ad0647285d44e9d58af185c
MjStryker <mjstryker51@gmail.com>
2025-01-30 22:28:09 +0100

    Add EventServiceProvider

--- src/Presentation/App.tsx
+++ src/Presentation/App.tsx
@@ -2,15 +2,18 @@ import { ChakraProvider, Flex } from '@chakra-ui/react';
 
 import DataTable from './Pages/DataTable';
 import UseGlobalHooks from '@/Application/@Global/UseGlobalHooks';
+import { EventServiceProvider } from '@/@Event/EventService';
 
 export default function App() {
   UseGlobalHooks();
 
   return (
-    <ChakraProvider>
-      <Flex className="AppContainer" minW="full" minH="100svh" justifyContent="center">
-        <DataTable />
-      </Flex>
-    </ChakraProvider>
+    <EventServiceProvider>
+      <ChakraProvider>
+        <Flex className="AppContainer" minW="full" minH="100svh" justifyContent="center">
+          <DataTable />
+        </Flex>
+      </ChakraProvider>
+    </EventServiceProvider>
   );
 }
