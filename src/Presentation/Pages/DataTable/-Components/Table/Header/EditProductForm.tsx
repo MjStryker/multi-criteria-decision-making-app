95363a66dff02fe16ab07df612c1174fa73d6e00
MjStryker <mjstryker51@gmail.com>
2025-01-30 22:32:10 +0100

    Fix cannot delete product

--- src/Presentation/Pages/DataTable/-Components/Table/Header/EditProductForm.tsx
+++ src/Presentation/Pages/DataTable/-Components/Table/Header/EditProductForm.tsx
@@ -1,10 +1,9 @@
-import { isValidNotEmptyString } from '@/@Shared/@Utils/String';
 import UseRemoveProductCommand from '@/Application/Product/Commands/UseRemoveProduct.command';
 import UseUpdateProductCommand from '@/Application/Product/Commands/UseUpdateProduct.command';
 
 import { ProductDto } from '@/Application/Product/Dtos/Product.dto';
 import { DeleteIcon } from '@chakra-ui/icons';
-import { Button, HStack, IconButton, Stack, Text } from '@chakra-ui/react';
+import { Button, HStack, IconButton, Stack } from '@chakra-ui/react';
 import { Dispatch, FormEvent, SetStateAction, useEffect, useState } from 'react';
 import TextInput from '../../../../../Components/Form/TextInput';
 
@@ -21,8 +20,6 @@ export default function EditProductForm({ setParentIsDirty, product, onParentClo
   const [name, setName] = useState<string>(product.name || '');
 
   const [reference, setReference] = useState<string>(product.reference || '');
-
-  const [confirmDelete, setConfirmDelete] = useState(false);
 
   const isDirty = name !== product.name || reference !== product.reference;
 
@@ -56,7 +53,6 @@ export default function EditProductForm({ setParentIsDirty, product, onParentClo
    * * Dialog actions
    */
   const onClose = () => {
-    setConfirmDelete(false);
     onParentClose();
   };
 
@@ -81,14 +77,7 @@ export default function EditProductForm({ setParentIsDirty, product, onParentClo
   return (
     <form onSubmit={onSubmit}>
       <Stack spacing={4}>
-        <TextInput
-          label="Name"
-          id="product-name"
-          type="text"
-          value={name}
-          onChange={onNameChange}
-          isDisabled={confirmDelete}
-        />
+        <TextInput label="Name" id="product-name" type="text" value={name} onChange={onNameChange} />
 
         <TextInput
           label="Reference"
@@ -96,48 +85,18 @@ export default function EditProductForm({ setParentIsDirty, product, onParentClo
           type="text"
           value={reference}
           onChange={onReferenceChange}
-          isDisabled={confirmDelete}
         />
 
         <HStack>
-          {confirmDelete ? (
-            <>
-              <Text flex={2} fontSize="md">
-                Delete?
-              </Text>
+          <IconButton colorScheme="red" icon={<DeleteIcon />} aria-label="Delete product" onClick={() => onDelete()} />
 
-              <Button flex={1} variant="outline" onClick={() => setConfirmDelete(false)}>
-                No
-              </Button>
+          <Button flex={1} variant="outline" onClick={onClose}>
+            Cancel
+          </Button>
 
-              <Button flex={1} colorScheme="red" onClick={onDelete}>
-                Yes
-              </Button>
-            </>
-          ) : (
-            <>
-              <IconButton
-                colorScheme="red"
-                icon={<DeleteIcon />}
-                aria-label="Delete product"
-                onClick={() =>
-                  !isValidNotEmptyString(name) && !isValidNotEmptyString(reference)
-                    ? // * If fields are empty + product does not have any value
-                      onDelete
-                    : // * Else go through confirm process
-                      setConfirmDelete(true)
-                }
-              />
-
-              <Button flex={1} variant="outline" onClick={onClose}>
-                Cancel
-              </Button>
-
-              <Button flex={1} type="submit" colorScheme="teal" isDisabled={!isDirty}>
-                Save
-              </Button>
-            </>
-          )}
+          <Button flex={1} type="submit" colorScheme="teal" isDisabled={!isDirty}>
+            Save
+          </Button>
         </HStack>
       </Stack>
     </form>
