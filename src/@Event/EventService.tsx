98a5238e711d3fa7a1a2caf6b7ea29058b23555d
MjStryker <mjstryker51@gmail.com>
2025-02-03 10:56:11 +0100

    Remove not working event service provider

--- src/@Event/EventService.tsx
+++ src/@Event/EventService.tsx
@@ -1,44 +1,20 @@
 import { AbstractEvent } from './AbstractEvent';
 
-export const EVENT_SERVICE_CONTAINER_ID = 'event-service-container';
-
 export class EventService {
   static container: HTMLElement | null = null;
 
-  static provider(): HTMLElement {
-    if (this.container === null) {
-      this.container = document.getElementById(EVENT_SERVICE_CONTAINER_ID);
-    }
-    if (this.container === null) {
-      throw new Error('EventServiceProvider not found');
-    }
-    return this.container;
-  }
-
   static emit<T>(event: AbstractEvent<T>) {
     console.log('event >> emit', event);
-    EventService.provider().dispatchEvent(event);
+    window.dispatchEvent(event);
   }
 
   static subscribe<T>(eventName: string, listener: (event: AbstractEvent<T>) => void) {
     console.log('event >> subscribe', { eventName });
-    EventService.provider().addEventListener(eventName, e => listener(e as AbstractEvent<T>));
+    window.addEventListener(eventName, e => listener(e as AbstractEvent<T>));
   }
 
   static unsubscribe<T>(eventName: string, listener: (event: AbstractEvent<T>) => void) {
     console.log('event >> unsubscribe', { eventName });
-    EventService.provider().removeEventListener(eventName, e => listener(e as AbstractEvent<T>));
+    window.removeEventListener(eventName, e => listener(e as AbstractEvent<T>));
   }
-}
-
-export function EventServiceProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <div
-        id={EVENT_SERVICE_CONTAINER_ID}
-        style={{ position: 'fixed', top: 0, left: 0, zIndex: -1, pointerEvents: 'none' }}
-      />
-      {children}
-    </>
-  );
 }
--- src/Presentation/App.tsx
+++ src/Presentation/App.tsx
@@ -1,19 +1,16 @@
 import { ChakraProvider, Flex } from '@chakra-ui/react';
 
-import DataTable from './Pages/DataTable';
 import UseGlobalHooks from '@/Application/@Global/UseGlobalHooks';
-import { EventServiceProvider } from '@/@Event/EventService';
+import DataTable from './Pages/DataTable';
 
 export default function App() {
   UseGlobalHooks();
 
   return (
-    <EventServiceProvider>
-      <ChakraProvider>
-        <Flex className="AppContainer" minW="full" minH="100svh" justifyContent="center">
-          <DataTable />
-        </Flex>
-      </ChakraProvider>
-    </EventServiceProvider>
+    <ChakraProvider>
+      <Flex className="AppContainer" minW="full" minH="100svh" justifyContent="center">
+        <DataTable />
+      </Flex>
+    </ChakraProvider>
   );
 }
